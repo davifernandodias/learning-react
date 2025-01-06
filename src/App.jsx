74d935ce833a9982c36c1990177c10300ecb989f
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-06 14:18:52 -0300

    initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-// import UserDetails from './components/UserDetails'
-import Form from './components/form/FormComponent'
 function App() {
 
-  const data = {
-    username: "davi",
-    email: "davifernando@gmail.com"
-  }
 
   return (
-    <Form  useremail={data.email} username={data.username}/>
-    // <div>
-    //   <UserDetails peoples={peoples} />
-    // </div>
+    <h1>oi</h1>
   )
 }
 
